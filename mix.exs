168e34826dbd630f597539fa6db42e53eef6e32a
Jay Hayes <ur@iamvery.com>
2015-08-25 08:13:14 -0500

    Restructure list for better commit diffs ᕕ( ᐛ )ᕗ

--- mix.exs
+++ mix.exs
@@ -29,11 +29,13 @@ defmodule InvestmentTime.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.17"},
-     {:phoenix_ecto, "~> 1.1"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.1"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 0.17"},
+      {:phoenix_ecto, "~> 1.1"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.1"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:cowboy, "~> 1.0"},
+    ]
   end
 end
