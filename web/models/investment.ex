f106ddf5f2af03a78c00b85b9d9ae003cc2fb080
Jay Hayes <ur@iamvery.com>
2015-08-28 13:47:28 -0500

    Add investment model

---
+++ web/models/investment.ex
@@ -0,0 +1,24 @@
+defmodule InvestmentTime.Investment do
+  use InvestmentTime.Web, :model
+
+  schema "investments" do
+    belongs_to :user, User
+    field :used, :boolean
+
+    timestamps
+  end
+
+  @required_fields ~w(user_id used)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
